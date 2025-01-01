7fe144a15da6e0fb62c36a17216d8521ad56bf0a
Dac Chuong <chuongpham.2905200@gmail.com>
2025-01-01 18:13:09 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import '../public/assets/css/style.css'
-import Detail from './pages/Detail'
-import Layout from './components/Layout'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import "../public/assets/css/style.css";
+import Detail from "./pages/Detail";
+import Layout from "./components/Layout";
+import { MantineProvider } from "@mantine/core";
+import '@mantine/core/styles.css';
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="detail" element={<Detail />} />
-          
-        </Route>
-      </Routes>
+      <MantineProvider defaultColorScheme="light">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="detail" element={<Detail />} />
+          </Route>
+        </Routes>
+      </MantineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
